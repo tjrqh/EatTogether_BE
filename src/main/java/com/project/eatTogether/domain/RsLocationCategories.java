b39d9d1786067c994952f1bc5403963ac10e909f
tjrqh <asdww6631@gmail.com>
2024-12-03 23:54:55 +0900

    fix : Entity 및 ORM, 데이터 타입 수정

---
+++ src/main/java/com/project/eatTogether/domain/RsLocationCategories.java
@@ -0,0 +1,16 @@
+package com.project.eatTogether.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class RsLocationCtegories {
+
+    
+
+}
