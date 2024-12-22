a0f4570c51c3b9fc2ca0c452be8c15c859eb7c35
tjrqh <asdww6631@gmail.com>
2024-12-23 02:31:25 +0900

    fix : 계정관리(유저) 운영시간 제외

--- src/main/java/com/project/eatTogether/presentation/RsRestaurantDetailController.java
+++ src/main/java/com/project/eatTogether/presentation/RsRestaurantDetailController.java
@@ -1,14 +1,17 @@
 package com.project.eatTogether.presentation;
 
 import com.project.eatTogether.application.dto.RsRestaurantDetailDTO;
+import com.project.eatTogether.application.dto.restaurantDto.RestaurantModifyReadResponse;
 import com.project.eatTogether.application.service.RsRestaurantDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/restaurant-details")
 @RequiredArgsConstructor
 public class RsRestaurantDetailController {
 
@@ -19,5 +22,13 @@ public class RsRestaurantDetailController {
                                                       @RequestParam(defaultValue = "0") int page,
                                                       @RequestParam(defaultValue = "10") int size) {
         return restaurantDetailService.getRestaurantDetails(rsId, page, size);
+    }
+
+    //Owner 창 메뉴 불러오기
+    @GetMapping("/store")
+    public RestaurantModifyReadResponse getRestaurantDetails() {
+        Long userId=1L;
+        return restaurantDetailService.getRestaurantModifyReadResponse(userId);
+
     }
 }
